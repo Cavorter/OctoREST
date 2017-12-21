992718b9daa33be9d36bcf7b4089b73063a78e89
Nathan Stohlmann <nathan.stohlmann@verifybrand.com>
2017-12-21 14:54:45 -0600

    Added function Get-TenantImage

---
+++ Functions/Get-TenantImage.Tests.ps1
@@ -0,0 +1,42 @@
+﻿$functionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Split('.')[0]
+. "$PSScriptRoot\$functionName.ps1"
+
+Set-Alias -Name Test-Function -Value $functionName -Scope Script
+
+. $PSScriptRoot\..\TestContent\CommonTests.ps1
+
+Describe "$functionName" {
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+
+	$goodParams.Id = $goodTenant
+
+	Test-StandardParameters
+
+	Context "Happy Path" {
+		Test-Function @commonParams @goodParams
+		$assertParams = @{ CommandName = "Invoke-RestMethod"; Times = 1; Exactly = [switch]$true }
+	
+		It "Passes the value of the Id parameter" {
+			$assertParams.ParameterFilter = { $Uri -like "*tenants/$goodTenant/*" }
+			Assert-MockCalled @assertParams
+		}
+	
+		It "requests the logo endpoint" {
+			$assertParams.ParameterFilter = { $Uri -like "*/logo" }
+			Assert-MockCalled @assertParams
+		}
+	
+		It "defaults to a local file called logo.png for the downloaded image file" {
+			$assertParams.ParameterFilter = { $OutFile -eq ".\logo.png" }
+			Assert-MockCalled @assertParams
+		}
+	
+		It "outputs the image to the file specified in the OutFile parameter" {
+			$customOutFile = "{0}\{1}.png" -f $TestDrive, $goodTenant
+			$assertParams.ParameterFilter = { $OutFile -eq $customOutFile }
+			$assertParams.Scope = "It"
+			Test-Function @commonParams @goodParams -OutFile $customOutFile
+			Assert-MockCalled @assertParams
+		}
+	}
+}
---
+++ Functions/Get-TenantImage.ps1
@@ -0,0 +1,42 @@
+﻿function Get-TenantImage {
+	<#
+		.SYNOPSIS
+			Retrieves the image associated with a Tenant
+		.DESCRIPTION
+			Uses the Octopus Deploy REST API to retrieve the logo image associated with a specific tenant.
+		.PARAMETER Id
+			The ID of a tenant to retrieve the image for
+		.PARAMETER OutFile
+			The name and path of where to save the image file. If not specified defaults to a file named "logo.png" in the current path.
+		.PARAMETER Server
+			The URI for an Octopus Deploy server
+		.PARAMETER APIKey
+			The API key for a user with the rights to view tenant information
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Id,
+		
+		[Parameter(Mandatory=$false)]
+		[string]$OutFile = ".\logo.png",
+		
+		[Parameter(Mandatory=$true)]
+		[string]$Server,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$APIKey
+	)
+	
+	Begin {
+		[uri]$uri = "$Server/api/tenants/$Id/logo"
+		Write-Verbose "URI: $($uri.AbsoluteUri)"
+		$headers = @{ 'X-Octopus-ApiKey' = $APIKey }
+		Write-Verbose "OutFile: $OutFile"
+	}
+	
+	Process {
+		$result = Invoke-RestMethod -UseBasicParsing -Uri $uri.AbsoluteUri -Headers $headers -OutFile $OutFile
+		Write-Output $result
+	}
+}
