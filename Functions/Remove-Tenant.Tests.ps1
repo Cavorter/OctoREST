cc7c376f73814a7f11e0058905d015fe6452aa66
Nathan Stohlmann <nathan.stohlmann@verifybrand.com>
2017-12-21 15:30:33 -0600

    Added Remove-Tenant function

---
+++ Functions/Remove-Tenant.Tests.ps1
@@ -0,0 +1,29 @@
+﻿$functionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Split('.')[0]
+. "$PSScriptRoot\$functionName.ps1"
+
+Set-Alias -Name Test-Function -Value $functionName -Scope Script
+
+. $PSScriptRoot\..\TestContent\CommonTests.ps1
+
+Describe "$functionName" {
+	$goodParams.Id = $goodTenant
+
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+
+	Test-StandardParameters
+
+	Context "Id Parameter" {
+		Test-Function @commonParams @goodParams
+		$assertParams = @{ CommandName = "Invoke-RestMethod"; Times = 1; Exactly = [switch]$true; Scope = "Context" }
+	
+		It "Passes the value of the Id parameter if specified" {
+			$assertParams.ParameterFilter = { $Uri -like "*tenants/$goodTenant" }
+			Assert-MockCalled @assertParams
+		}
+	
+		It "Specifies the Delete method for Invoke-RestMethod" {
+			$assertParams.ParameterFilter = { $Method -eq "Delete" }
+			Assert-MockCalled @assertParams
+		}
+	}
+}
---
+++ Functions/Remove-Tenant.ps1
@@ -0,0 +1,38 @@
+﻿function Remove-Tenant {
+	<#
+		.SYNOPSIS
+			Deletes a tenant from an Octopus Deploy server.
+		.DESCRIPTION
+			Connects to an Octopus Deploy server and deletes a tenant with the specified Id.
+			Implements the Delete method for the /api/tenants endpoint.
+		.PAREMETER Id
+			The ID of a tenant to delete
+		.PARAMETER Server
+			The URI for an Octopus Deploy server
+		.PARAMETER APIKey
+			The API key for a user with the rights to view tenant information
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Id,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$Server,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$APIKey
+	)
+	
+	Begin {
+		[uri]$uri = "$Server/api/tenants/$Id"
+		Write-Verbose "URI: $($uri.AbsoluteUri)"
+		$headers = @{ 'X-Octopus-ApiKey' = $APIKey }
+	}
+	
+	Process {
+		$result = Invoke-RestMethod -UseBasicParsing -Uri $uri.AbsoluteUri -Headers $headers -Method Delete
+		Write-Verbose "Result: $($result | ConvertTo-Json -Depth 100 -Compress)"
+		Write-Output $result
+	}
+}
