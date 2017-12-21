6a9427d09308f0e819372beed0944e6318c89217
Nathan Stohlmann <nathan.stohlmann@verifybrand.com>
2017-12-21 15:26:35 -0600

    Added New-Tenant function

---
+++ Functions/New-Tenant.Tests.ps1
@@ -0,0 +1,27 @@
+﻿$functionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Split('.')[0]
+. "$PSScriptRoot\$functionName.ps1"
+
+Set-Alias -Name Test-Function -Value $functionName -Scope Script
+
+. $PSScriptRoot\..\TestContent\CommonTests.ps1
+
+Describe "$functionName" {
+	$goodParams.Name = $goodTenant
+
+	Mock -CommandName Get-OctoTenant -MockWith { return $false }
+	Mock -CommandName Invoke-RestMethod -MockWith { return $true }
+
+	Test-StandardParameters
+
+	Context "Name Parameter" {
+		It "Passes the value of the Name parameter" {
+			Test-Function @commonParams @goodParams
+			Assert-MockCalled -CommandName Invoke-RestMethod -Times 1 -Scope It -ParameterFilter { $Body -and ( ( $Body | ConvertFrom-Json ).Name -eq $goodTenant ) }
+		}
+	
+		It "Validates the value of the Name parameter" {
+			Mock -CommandName Get-OctoTenant -MockWith { return ( New-Object -TypeName PsObject -Property @{ Name = $goodTenant } ) }
+			{ Test-Function @commonParams @goodParams } | Should Throw
+		}
+	}
+}
---
+++ Functions/New-Tenant.ps1
@@ -0,0 +1,42 @@
+﻿function New-Tenant {
+	<#
+		.SYNOPSIS
+			Creates a new tentant on an Octopus Deploy server
+		.DESCRIPTION
+			Verifies that an existing tenant with the same name does not already exist, and then proceeds to create a new tenant on the specified Octopus Deploy server.
+		.PARAMETER Name
+			The name of the tenant to create
+		.PARAMETER Server
+			The URI for an Octopus Deploy server
+		.PARAMETER APIKey
+			The API key for a user with the rights to view project information
+	#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true)]
+		[string]$Server,
+		
+		[Parameter(Mandatory=$true)]
+		[string]$APIKey,
+
+		[Parameter(Mandatory=$true)]
+		[string]$Name
+	)
+	
+	$ErrorActionPreference = "Stop"
+	
+	#validate tenant does not already exist
+	if ( Get-OctoTenant -Server $Server -APIKey $APIKey | Where-Object { $_.Name -eq $Name } ) {
+		throw "Specified tenant already exists! ($Name)"
+	} else {
+		Write-Verbose "Validated tenant does not already exist. ($Name)"
+	}
+
+	[uri]$uri = "$Server/api/tenants"
+	Write-Verbose "URI: $($uri.AbsoluteUri)"
+	$headers = @{ 'X-Octopus-ApiKey' = $APIKey }
+	$body = @{ Name = $name } | ConvertTo-Json -Compress
+	$result = Invoke-RestMethod -UseBasicParsing -Uri $uri.AbsoluteUri -Headers $headers -Method Post -Body $body
+	Write-Debug "Result: $($result | ConvertTo-Json -Depth 100 -Compress)"
+	Write-Output $result
+}
